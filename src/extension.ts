c5fdc35f8f4995b15005dbd608ff45f018ae8c1b
iiheng <1269305589@qq.com>
2024-04-20 19:11:05 +0800

    feat: 可运行

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,124 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+'use strict';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
+import * as vscode from 'vscode';
+const fs = require('fs');
+const path = require('path');
+
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "file-mind-map-generator" is now active!');
+    let disposable = vscode.commands.registerCommand('extension.generateMap', (el) => {
+        // Display a Tree text
+        let tree = '<bold><span class="t-icon" name="icons">📦</span>' + path.basename(el.fsPath) 
+                        + '</bold><br>' + entryTrees(el.fsPath, 0);
+        let tree_col = vscode.window.createWebviewPanel('text', 'Generate Tree Text',
+            { 
+                viewColumn: vscode.ViewColumn.Active 
+            },
+            {
+                enableScripts: true
+            });
+        tree_col.webview.html = OutputElements.Template.replace('--REP--', tree);
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('file-mind-map-generator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from file mind map generator!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+}
+
+// directory and file ditective function
+export function entryTrees (trgPath: string, deps: number) {
+    let treeText = '';
+    if (!fs.existsSync(trgPath)) { return ''; }
+
+    // string format for tree text
+    let format = function (deps: number, pipe: string, name: string) {
+        return ' ' + Array(deps + 1).join('┃ ') + pipe + name + '<br>';
+    };
+
+    // order by directory > file
+    let beforSortFiles: Array<object> = fs.readdirSync(trgPath);
+    let paths: Array<object> = [];
+
+    let tmpFiles: Array<object> = [];
+    beforSortFiles.forEach(target => {
+        let fullPath = path.join(trgPath, target.toString());
+        if (fs.statSync(fullPath).isDirectory()) {
+            paths.push(target);
+        } else {
+            tmpFiles.push(target);
+        }
+    });
+    paths = paths.concat(tmpFiles);
+
+    paths.forEach(item => {
+        let fullPath = path.join(trgPath, item.toString());
+        let pipe = paths.indexOf(item) === paths.length - 1 ? '┗ ' : '┣ ';
+        
+        if (fs.statSync(fullPath).isDirectory()) {
+            treeText += format(deps, pipe, '<span class="t-icon" name="icons">📂</span>' + item.toString());
+            treeText += entryTrees(fullPath, deps + 1);
+        } else {
+            treeText += format(deps, pipe, '<span class="t-icon" name="icons">📜</span>' + item.toString());
+        }
+    });
+    return treeText;
+}
+
+class OutputElements {
+    static Template = `
+    <html>
+      <head>
+      <style>
+      .btn-ftg {
+        position: relative;
+        display: inline-block;
+        padding: 0.25em 0.5em;
+        text-decoration: none;
+        color: #FFF;
+        background: #3595fd;
+        border-bottom: solid 2px #007dd2;
+        border-radius: 4px;
+        box-shadow: inset 0 2px 0 rgba(255,255,255,0.2), 0 2px 2px rgba(0, 0, 0, 0.19);
+        font-weight: bold;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+         -ms-user-select: none;
+             user-select: none;
+
+        &:active {
+          border-bottom: solid 2px #3595fd;
+          box-shadow: 0 0 2px rgba(0, 0, 0, 0.30);
+        }
+      }
+      </style>
+      </head>
+      <body>
+        <div class="head">
+          <div id="icon-switch" class="btn-ftg"
+           onclick="switchVisibility();">
+          icon off
+          </div>
+        </div>
+        <pre id="tree-panel">--REP--</pre>
+        <script type="text/javascript">
+            function switchVisibility() {
+                var mode = document.getElementById("icon-switch").textContent;
+                var iconList = document.getElementsByName("icons");
+                var visible = "inline";
+                if (mode != 'icon on') {
+                    document.getElementById("icon-switch").textContent = 'icon on'
+                    visible = "none";
+                } else {
+                    document.getElementById("icon-switch").textContent = 'icon off'
+                }
+                iconList.forEach(icon => {
+                    icon.style.display = visible;
+                })
+                window.focus();
+            }
+        </script>
+      </body>
+    </html>
+    `;
+}
